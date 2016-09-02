cd32615f1473f5b3a63631833d020e6686610d0a
Sascha Wessel <swessel@gr4yweb.de>
2016-09-02 17:40:07 +0200

    geez... so maybe now it is able to find the save file?

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ ! -f /opt/factorio/saves/save0.zip ]; then
-  /opt/factorio/bin/x64/factorio --create saves/save0
+  /opt/factorio/bin/x64/factorio --create saves/save0.zip
 fi
-/opt/factorio/bin/x64/factorio --start-server saves/save0
+/opt/factorio/bin/x64/factorio --start-server saves/save0.zip
