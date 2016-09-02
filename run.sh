fb0e0b2379319f5da867c63869c828497ce4ccbd
Sascha Wessel <swessel@gr4yweb.de>
2016-09-02 17:29:30 +0200

    fixed path to save in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ ! -f /opt/factorio/saves/save0.zip ]; then
-  /opt/factorio/bin/x64/factorio --create save0
+  /opt/factorio/bin/x64/factorio --create saves/save0
 fi
-/opt/factorio/bin/x64/factorio --start-server save0
+/opt/factorio/bin/x64/factorio --start-server saves/save0
